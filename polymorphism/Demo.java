26bca75561c4c966b8720c8df498322f254cce98
HarshithaDK1 <harshithadk85@gmail.com>
2025-09-15 18:21:34 +0530

    Add files via upload

---
+++ polymorphism/Demo.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p1=new Point();
+		System.out.println(p1);
+		Point p2=new Point(0.2f,0.5f);
+		System.out.println(p2);
+		Point p3=new Point(0.2f);
+		System.out.println(p3);
+		
+	}
+
+}
---
+++ polymorphism/Sbi.java
@@ -0,0 +1,14 @@
+package polymorphism;
+
+public class Sbi extends Rbi{
+
+	@Override
+	public float getRateOfInterest() {
+		// TODO Auto-generated method stub
+		super.getRateOfInterest();
+		return 8.5f;
+	}
+
+	
+
+}
